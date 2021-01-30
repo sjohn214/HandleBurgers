c4c7b92e5085b230e8fd9551a42584345a69a72c
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-01-29 20:21:13 -0500

    Add connection,orm,burgerscontroller, burger, server files and functions

---
+++ db/burgerDBSeeds.sql
@@ -0,0 +1,14 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("tofu burger", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("black bean burger", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("cheeseburger", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("turkey burger", false);
+
---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS burgers_db
+USE burgers_db
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY(id)
+);
