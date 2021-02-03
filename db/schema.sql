cff536150ded70590c7b950cf015a2ea97cd3c62
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-02 21:23:16 -0500

    Add update obj name

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,3 @@
 CREATE DATABASE IF NOT EXISTS burger_db;
 USE burger_db;
 
-CREATE TABLE burgers(
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY(id)
-);
